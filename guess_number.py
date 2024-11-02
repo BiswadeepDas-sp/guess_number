e8f41e4adca20c98152b63545be90b6222e347ea
BiswadeepDas-sp <82663470+BiswadeepDas-sp@users.noreply.github.com>
2024-11-02 13:44:12 +0530

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -2,97 +2,87 @@ import random
 
 class Number:
     def generate(self):
-        num=random.randint(1,100)
-        return num
-    
+        return random.randint(1, 100)
+
 class User:
-    def guess_input(self):
-        made_guess=False
-        while made_guess==False:
-           try:
-               print()
-               guess_num=int(input('Guess a number between 1 to 100:  '))
-               print()
-               made_guess=True
-           except:
-               print('Enter a valid response')
-        return guess_num
-    
-    def cont_exit(self):
-        valid=False
-        while valid==False:
-            try: 
-                choice=int(input('Continue(0) or Exit(1)   '))
-                valid=True
-            except:
-                print('You have entered an invalid response')
-        
-        if choice==0:
-            game.game_play()
+    def get_guess(self):
+        while True:
+            try:
+                guess = int(input('Guess a number between 1 and 100: '))
+                if 1 <= guess <= 100:
+                    return guess
+                else:
+                    print('Please enter a number between 1 and 100.')
+            except ValueError:
+                print('Invalid input. Please enter an integer.')
+
+    def get_choice(self):
+        while True:
+            try:
+                choice = int(input('Continue (0) or Exit (1): '))
+                if choice in (0, 1):
+                    return choice
+                else:
+                    print('Please enter 0 or 1.')
+            except ValueError:
+                print('Invalid input. Please enter 0 or 1.')
+
+class Game:
+    def __init__(self):
+        self.score = 0
+        self.guesses_left = 5
+        self.cheat = False
+
+    def calculate_score(self, guess, number):
+        if not self.cheat and self.guesses_left > 0:
+            return round(100 - (abs(guess - number) / number * 100), 0)
+        return 0
+
+    def game_play(self):
+        number = Number().generate()
+        user = User()
+        self.guesses_left = 5
+        self.cheat = False
+        win = False
+
+        print(f"The number is {number}")  # For testing; consider removing this line in production
+
+        while self.guesses_left > 0 and not win:
+            guess = user.get_guess()
+
+            if guess == 999:
+                print(f"The number is {number}")
+                self.cheat = True
+                continue
+
+            self.guesses_left -= 1
+
+            if guess == number:
+                print(f"\nYou have won in {5 - self.guesses_left} guess{'es' if 5 - self.guesses_left > 1 else ''}!")
+                win = True
+            else:
+                print("Wrong guess, try again!")
+                if self.guesses_left > 0:
+                    print(f"You have {self.guesses_left} guesses left.")
+
+            self.score += self.calculate_score(guess, number)
+            print(f"Score: {self.score}")
+            print("*" * 30)
+
+        if not win:
+            print("\nToo many guesses, better luck next time!")
+            print(f"The number was {number}")
+            print("*" * 30)
+
+        if self.cheat:
+            print("*" * 30)
+            print("HMMMM. Okay!")
+
+        if user.get_choice() == 0:
+            self.game_play()
         else:
             exit()
-            
-class Game:
-    def score(self):
-        if self.cheat==False and self.guess_left>0:
-             self.score_round=round(100-(abs(self.guess-self.num_gen)/self.num_gen*100),0)
-        return self.score_round
-        
-    def game_play(self):
-        self.guess_left=5
-        win=False
-        number=Number()
-        user=User()
-        self.num_gen=number.generate() 
-        self.cheat=False
-        score=0
-        #print(f'The number is {self.num_gen}')
-        while win==False and self.guess_left>0:
-           self.guess=user.guess_input()
-           if self.guess>100:
-            if self.guess==999:
-                print(f'The number is {self.num_gen}')
-                self.cheat=True
-            else:
-                print('Enter a number between 1 to 100')
-                print('*'*30)
-            continue
-           self.guess_left-=1
-           if self.num_gen==self.guess:
-                    
-                print()
-                if self.cheat==False:
-                    print()
-                    print(f'You have won in {5-self.guess_left} {'guesses' if 5-self.guess_left>1 else 'guess'}')
-                    print('*'*30)
-                    
-                win=True
-           else:
-                print('Wrong guess, try again!')
-                print()
-                print(f'You have {self.guess_left} guesses left')
-           score+=self.score()
-           print()
-           print(f'Score = {score}')
-           print('*'*30)
-                
-        if self.guess_left==0 and win==False:
-            print()
-            print('Too many guesses, Better luck next time!!')
-            print()
-            print(f'The number was {self.num_gen}')
-            print('*'*30)
-        try: 
-            if self.cheat==True:
-                print('*'*30)
-                print('HMMMM. Okay!')
-                
-        except:
-            pass
-       
-        user.cont_exit() 
-        print()
-        print('*'*30)
-    
-game=Game()
+
+# Start the game
+game = Game()
 game.game_play()
